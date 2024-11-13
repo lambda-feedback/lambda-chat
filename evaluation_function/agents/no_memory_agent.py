d689d20e4d68915d7d5945c083a0425152879ad2
neagualexa <ngu.alexandra@gmail.com>
2024-11-13 10:50:39 +0000

    feat: add summary of received message history

--- evaluation_function/agents/no_memory_agent.py
+++ evaluation_function/agents/no_memory_agent.py
@@ -11,40 +11,84 @@ from typing_extensions import TypedDict
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
+    summary: str
 
 class ChatbotNoMemoryAgent:
     def __init__(self):
         llm = OpenAILLMs()
         self.llm = llm.get_llm()
+        summarisation_llm = OpenAILLMs()
+        self.summarisation_llm = summarisation_llm.get_llm()
 
-        # Define a new graph for the conversation
+        # Define a new graph for the conversation & compile it
         self.workflow = StateGraph(State)
         self.workflow_definition()
-        # Finally, we compile it!
         self.app = self.workflow.compile()
 
     def call_model(self, state: State) -> str:
-        messages = state["messages"]
-        print(f"DEBUGGING: {messages}")
+        """Call the LLM model."""
+        summary = state.get("summary", "")
+        if summary:
+            system_message = f"Summary of conversation earlier: {summary}"
+            messages = [SystemMessage(content=system_message)] + state['messages']
+        else:
+            messages = state["messages"]
 
         valid_messages = self.check_for_valid_messages(messages)
         response = self.llm.invoke(valid_messages)
 
-        # We return a list, because this will get added to the existing list
         return {"messages": [response]}
     
     def check_for_valid_messages(self, messages):
-        # Removing the RemoveMessage() from the list of messages
+        """ Removing the RemoveMessage() from the list of messages """
         valid_messages = []
         for message in messages:
             if message.type != 'remove':
                 valid_messages.append(message)
         return valid_messages
     
+    def summarize_conversation(self, state: State):
+        """Summarize the conversation."""
+        summary = state.get("summary", "")
+        if summary:
+            summary_message = (
+                f"This is summary of the conversation to date: {summary}\n\n"
+                "Update the summary by taking into account the new messages above:"
+            )
+        else:
+            # summary_message = "Create a summary of the conversation above:"
+            summary_message = "Identify the key conversational preferences of the human user in the conversation above. Do not to focus on the details of the conversation:"
+
+        messages = state["messages"] + [SystemMessage(content=summary_message)] 
+        valid_messages = self.check_for_valid_messages(messages)
+        summary_response = self.summarisation_llm.invoke(valid_messages)
+        # Delete messages that are no longer wanted, except the last ones
+        delete_messages = [RemoveMessage(id=m.id) for m in state["messages"][:-3]]
+
+        return {"summary": summary_response.content, "messages": delete_messages}
+    
+    def should_summarize(self, state: State) -> str:
+        """Return the next node to execute.
+        If there are more than X messages, then we summarize the conversation.
+        Otherwise, we call the LLM."""
+
+        messages = state["messages"]
+        valid_messages = self.check_for_valid_messages(messages)
+        nr_messages = len(valid_messages)
+        if "system" in valid_messages[-1].type:
+            nr_messages -= 1
+
+        # always pairs of (sent, response) + 1 latest message
+        if nr_messages > 11:
+            return "summarize_conversation"
+        return "call_llm"    
+
     def workflow_definition(self):
         self.workflow.add_node("call_llm", self.call_model)
+        self.workflow.add_node("summarize_conversation", self.summarize_conversation)
 
-        self.workflow.add_edge(START, "call_llm")
+        self.workflow.add_conditional_edges(source=START, path=self.should_summarize)
+        self.workflow.add_edge("summarize_conversation", "call_llm")
         self.workflow.add_edge("call_llm", END)
     
     def print_update(self, update):
@@ -55,7 +99,6 @@ class ChatbotNoMemoryAgent:
                 print(v["summary"])
 
     def pretty_response_value(self, event):
-        # print(event["messages"][-1])
         return event["messages"][-1].content
     
 
@@ -74,6 +117,10 @@ if __name__ == "__main__":
         {"content": "London is the capital of England.", "type": "ai"},
         {"content": "What about dogs?", "type": "human"},
         {"content": "Dogs are the favorite pets of humans.", "type": "ai"},
+        {"content": "cats", "type": "human"},
+        {"content": "Cats are the second favorite pets of humans.", "type": "ai"},
+        {"content": "Paris?", "type": "human"},
+        {"content": "Paris is the capital of France.", "type": "ai"},
     ]
 
     def stream_graph_updates(user_input: str):
